e325c4745a511e0a1cafdb9b25e06430139ee870
Matthew Robbins <mrobbins_99@hotmail.com>
2015-06-24 21:47:11 +1000

    Add url into assembly version. Bump version to 0.0.3

--- XamarinResourceStringExtractor/Properties/AddinInfo.cs
+++ XamarinResourceStringExtractor/Properties/AddinInfo.cs
@@ -5,10 +5,11 @@ using Mono.Addins.Description;
 [assembly:Addin (
 	"Xamarin.Android Resource String Extractor", 
 	Namespace = "XamarinResourceStringExtractor",
-	Version = "0.0.2"
+	Version = "0.0.3"
 )]
 
 [assembly:AddinName ("Xamarin.Android Resource String Extractor")]
 [assembly:AddinCategory ("IDE extensions")]
-[assembly:AddinDescription ("A small tool to extract string literals within Activities and Fragments into the strings resource file.\n    \nThis is a sample application for a wider set of refactoring tools for Xamarin.Android. Please submit bug reports and feature requests to:\nhttps://github.com/matthew-ch-robbins/xamarin-android-string-extractor\n    \nTo help shape the direction of this project, connect with me on LinkedIn and have a chat:\nhttps://au.linkedin.com/pub/matthew-robbins/17/8a7/139")]
+[assembly:AddinDescription ("A small tool to extract string literals within Activities and Fragments into the strings resource file.\n    \nThis is a sample application for a wider set of refactoring tools for Xamarin.Android. Please submit bug reports to:\nhttps://github.com/matthew-ch-robbins/xamarin-android-string-extractor\n    \nTo help shape the direction of this project, connect with me on LinkedIn and have a chat:\nhttps://au.linkedin.com/pub/matthew-robbins/17/8a7/139")]
+[assembly:AddinUrl("https://github.com/matthew-ch-robbins/xamarin-android-string-extractor")]
 [assembly:AddinAuthor ("Matthew Robbins")]
